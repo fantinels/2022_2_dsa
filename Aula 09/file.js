20e727626779c214029033077ed1a9a13650cdf2
paolafantinel <94869859+fantinels@users.noreply.github.com>
2022-10-07 09:16:56 -0300

    Callback, Promise, Async/Await

---
+++ Aula 09/file.js
@@ -0,0 +1,24 @@
+fs = require('fs');
+fs.writeFile('teste.txt', 'Hello World!', function (err) {
+  if (err) return console.log(err);
+  //console.log('Hello World > teste.txt');
+});
+
+console.log('Hello World')
+
+let myPromise = new Promise(function(myResolve, myReject) {
+    let x = 0
+
+    if (x == 0) {
+        myResolve('Deu certo!');
+    } else {
+        myReject('Deu erro!');
+    }
+      
+
+});
+    
+    myPromise.then(
+      function(value) { console.log(value) },
+      function(error) { console.log(error) }
+    );
