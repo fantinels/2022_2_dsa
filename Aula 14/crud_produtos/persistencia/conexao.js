e1ece18da2cf6017adcf92cbaf7623879c8c5c06
paolafantinel <94869859+fantinels@users.noreply.github.com>
2022-11-30 10:06:19 -0300

    Adicionando Categoria

---
+++ Aula 14/crud_produtos/persistencia/conexao.js
@@ -0,0 +1,10 @@
+const conexao = {
+    host: 'localhost',
+    port: 5432,
+    database: 'crud_produtos',
+    user: 'postgres',
+    // password:'postgres'
+    password:'1234'
+}
+
+module.exports = {conexao}
